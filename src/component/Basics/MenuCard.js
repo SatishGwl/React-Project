3537a6830ab9090f65b50e0889429475c833296b
SatishGwl <satish.srcem@gmail.com>
2021-12-28 16:50:17 +0530

    add navbar and filter item based on category click

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -4,29 +4,29 @@ const MenuCard = ({ menuData }) => {
     // console.log('==menuData==', menuData);
     return (
         <>
-        <section className="main-card--cointainer">
-        {
-                menuData.map((currentElem, index) => {
-                    return (
-                        <>
-                            <div className="card-container" key={currentElem.id}>
-                                <div className="card">
-                                    <div className="card-body">
-                    <span className="card-number card-circle subtle">{currentElem.id}</span>
-                    <span className="card-author subtle">{currentElem.name}</span>
-                                        <h2 className="card-title">{currentElem.name}</h2>
-                    <span className="card-description subtle">{currentElem.description}</span>
-                                        <div className="card-read">Read</div>
+            <section className="main-card--cointainer">
+                {
+                    menuData.map((currentElem, index) => {
+                        return (
+                            <>
+                                <div className="card-container" key={currentElem.id}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <span className="card-number card-circle subtle">{currentElem.id}</span>
+                                            <span className="card-author subtle">{currentElem.name}</span>
+                                            <h2 className="card-title">{currentElem.name}</h2>
+                                            <span className="card-description subtle">{currentElem.description}</span>
+                                            <div className="card-read">Read</div>
+                                        </div>
+                                        <img src={currentElem.image} alt="images" className="card-media" />
+                                        <span className="card-tag subtle">Order Now</span>
                                     </div>
-                                    {/* <img src={image} alt="images" className="card-media" /> */}
-                                    <span className="card-tag subtle">Order Now</span>
                                 </div>
-                            </div>
-                        </>
-                    )
-                })
-            }
-        </section>
+                            </>
+                        )
+                    })
+                }
+            </section>
         </>
     )
 }
--- src/component/Basics/Resturent.js
+++ src/component/Basics/Resturent.js
@@ -5,9 +5,27 @@ import './style.css'
 
 const Resturent = () => {
   const [menuData, setMenuData] = useState(Menu);
-  console.log(menuData)
+  const filterItem = (category) => {
+    // filter data from Menu 
+    const updatedList = Menu.filter((currenElem) => {
+      // console.log('==updatedList==', updatedList);
+      return currenElem.category === category;
+    });
+    // set updateList Data 
+    setMenuData(updatedList);
+  };
   return (
     <>
+    <nav className="navbar">
+      <div className="btn-group">
+        <button className="btn-group__item" onClick={()=> filterItem('breakfast')}>Breakfast</button>
+        <button className="btn-group__item" onClick={()=> filterItem('lunch')}>Lunch</button>
+        <button className="btn-group__item" onClick={()=> filterItem('evening')}>Evening</button>
+        <button className="btn-group__item" onClick={()=> filterItem('dinner')}>Dinner</button>
+        <button className="btn-group__item" onClick={()=> setMenuData(Menu)}>All</button>
+
+      </div>
+    </nav>
       <MenuCard menuData = {menuData}/>
     </>
   )
