5bbb715d1f78c8e57e050bfd5eb209a5ef3e17bc
SatishGwl <satish.srcem@gmail.com>
2021-12-28 16:06:29 +0530

    add discription

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -15,7 +15,7 @@ const MenuCard = ({ menuData }) => {
                     <span className="card-number card-circle subtle">{currentElem.id}</span>
                     <span className="card-author subtle">{currentElem.name}</span>
                                         <h2 className="card-title">{currentElem.name}</h2>
-                                        <span className="card-description subtle">I Love Maggie,  Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,</span>
+                    <span className="card-description subtle">{currentElem.description}</span>
                                         <div className="card-read">Read</div>
                                     </div>
                                     {/* <img src={image} alt="images" className="card-media" /> */}
